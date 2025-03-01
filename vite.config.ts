259f0b14c09e2145a79cb97ec81c6cfe168b4af5
thilak-r <thilak22005@gmail.com>
2025-03-01 23:24:47 +0530

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+
+
 export default defineConfig({
   plugins: [react()],
   base: "/vite-deploy/",
@@ -12,17 +14,5 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          threejs: ['three'],
-          lodash: ['lodash'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Increase the chunk size limit warning to 1MB
   },
 });
